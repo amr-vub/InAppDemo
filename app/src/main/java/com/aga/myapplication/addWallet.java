48aece85588a2f3ff45dbf07e24060eafb1892ca
amr-vub <112233amr>
2017-09-01 13:46:23 +0200

    Adding the ability to scan the CC  number

--- app/src/main/java/com/aga/myapplication/addWallet.java
+++ app/src/main/java/com/aga/myapplication/addWallet.java
@@ -2,6 +2,7 @@ package com.aga.myapplication;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
@@ -25,17 +26,20 @@ import android.widget.Toast;
 import com.wl.sips.inapp.sdk.pojo.AddCardResponse;
 import com.wl.sips.inapp.sdk.pojo.OrderResponse;
 
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
+
 import static com.aga.myapplication.MainActivity.imei;
 import static com.aga.myapplication.R.id.crdNbr;
 
 public class addWallet extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private static final int REQUEST_READ_PHONE_STATE = 1;
+    private static final int REQUEST_READ_PHONE_STATE = 1, MY_SCAN_REQUEST_CODE = 3;
 
     RedirectionResponse redirectionResponse=null;
     AddCardResponse addCardResponse=null;
     OrderResponse orderResponse = null;
-    Button addWl;
+    Button addWl, scan;
     EditText cardNumber ;
     EditText cardAlias;
     EditText cardCsc;
@@ -95,6 +99,62 @@ public class addWallet extends AppCompatActivity implements AdapterView.OnItemSe
                 asyncT.execute();
             }
         });
+        scan = (Button) findViewById(R.id.scan);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onScanPress(v);
+            }
+        });
+    }
+
+    public void onScanPress(View v) {
+        Intent scanIntent = new Intent(this, CardIOActivity.class);
+
+        // customize these values to suit your needs.
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, false); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false); // default: false
+
+        // MY_SCAN_REQUEST_CODE is arbitrary and is only used within this activity.
+        startActivityForResult(scanIntent, MY_SCAN_REQUEST_CODE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_SCAN_REQUEST_CODE) {
+            String resultDisplayStr;
+            if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
+                CreditCard scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
+
+                // Never log a raw card number. Avoid displaying it, but if necessary use getFormattedCardNumber()
+                resultDisplayStr = "Card Number: " + scanResult.getRedactedCardNumber() + "\n";
+                cardNumber.setText(scanResult.cardNumber);
+                // Do something with the raw number, e.g.:
+                // myService.setCardNumber( scanResult.cardNumber );
+
+                if (scanResult.isExpiryValid()) {
+                    resultDisplayStr += "Expiration Date: " + scanResult.expiryMonth + "/" + scanResult.expiryYear + "\n";
+                }
+
+                if (scanResult.cvv != null) {
+                    // Never log or display a CVV
+                    resultDisplayStr += "CVV has " + scanResult.cvv.length() + " digits.\n";
+                }
+
+                if (scanResult.postalCode != null) {
+                    resultDisplayStr += "Postal Code: " + scanResult.postalCode + "\n";
+                }
+            }
+            else {
+                resultDisplayStr = "Scan was canceled.";
+            }
+            // do something with resultDisplayStr, maybe display it in a textView
+            // resultTextView.setText(resultDisplayStr);
+            System.out.println("--------------" + resultDisplayStr);
+        }
+        // else handle other activity results
     }
 
     @Override
